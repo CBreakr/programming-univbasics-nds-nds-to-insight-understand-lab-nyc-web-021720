b7cad5da0af2648d901bb581de1bbcc1acfc7f85
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-16 00:43:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -24,8 +24,8 @@ def pretty_print_nds(nds)
   pp nds
 end
 
+# this is a weird method name
 def print_first_directors_movie_titles
-  
+  director = directors_database[0]
+  movies = director[:movies]
 end
-
-pretty_print_nds(directors_database)
